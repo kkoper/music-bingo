839b3bad83a9feb8123f776f0948b6da22711dff
Kevin <k.koper@hkonet.nl>
2021-02-09 22:41:01 +0100

    ssl true

--- src/server/data/mongo-config.ts
+++ src/server/data/mongo-config.ts
@@ -9,6 +9,7 @@ export const mongoConfig = {
    "database": process.env.mongoDB,
    synchronize: true,
    logging: true,
+   ssl: true,
    sslCA: [process.env.SSL_CERT],
    entities: ["dist/server/data/entities/*.js"]
 } as ConnectionOptions;
